140e14507856aa4e8d2a01f75ed47ac2ccac3723
Brayden Tomlinson <humortronic@gmail.com>
2024-11-04 12:23:19 +1030

    Added changes to code

---
+++ ex02/main.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/02 17:59:27 by btomlins          #+#    #+#             */
+/*   Updated: 2024/11/04 12:19:58 by btomlins         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
+
+int main()
+{
+    ClapTrap clap("Clappy");
+
+    clap.attack("Target1");
+    clap.takeDamage(3);
+    clap.beRepaired(5);
+
+    std::cout << "Creating ScavTrap..." << std::endl;
+    ScavTrap scav("Scavvy");
+    clap.attack("Target2");
+    clap.takeDamage(30);
+    clap.beRepaired(10);
+    scav.guardGate();
+    
+    return(0);
+}
